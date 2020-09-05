a9af288120678f246548f274c85c4a0ad6154f35
TeamDoubleA <52544416+TeamDoubleA@users.noreply.github.com>
2020-09-05 12:35:17 +0200

    Add files via upload

---
+++ Fruit Ninja/Fruit Ninja/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Fruit_Ninja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.statusStrip2 = new System.Windows.Forms.StatusStrip();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-200, -200);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(246, 176);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 750;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 20);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 502);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1104, 25);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "Top Score: 0\\n     Misses: 0";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(151, 20);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(-200, -200);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(75, 23);
+            this.axWindowsMediaPlayer1.TabIndex = 2;
+            // 
+            // statusStrip2
+            // 
+            this.statusStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip2.Location = new System.Drawing.Point(0, 480);
+            this.statusStrip2.Name = "statusStrip2";
+            this.statusStrip2.Size = new System.Drawing.Size(1104, 22);
+            this.statusStrip2.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(-200, -200);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(77, 59);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(-200, -200);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(75, 55);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.pictureBox3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox3_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1104, 527);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.statusStrip2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Fruit Ninja";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.StatusStrip statusStrip2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Fruit Ninja/Fruit Ninja/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+using Fruit_Ninja.Properties;
+using Microsoft.Win32.SafeHandles;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Fruit_Ninja
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        int score = 0;
+        int misses = 0;
+        int fruit1 = 0, fruit2 = 0, fruit3 = 0;
+        Boolean running = true;
+        Boolean sliceable1 = true, sliceable2 = true, sliceable3 = true;
+        WMPLib.WindowsMediaPlayer wmp = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer wmp1 = new WMPLib.WindowsMediaPlayer();
+        Boolean flag = true;
+        int highscore = int.Parse(Settings.Default["highscore"].ToString());
+        int counter;
+
+        public Form1()
+        {
+            wmp.URL = "start.mp3";
+            InitializeComponent();
+            this.Cursor = new Cursor(Properties.Resources.katana2.GetHicon());
+            pictureBox1.Image = Properties.Resources.apple;
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+            pictureBox2.Image = Properties.Resources.apple;
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.SizeMode = PictureBoxSizeMode.Normal;
+            pictureBox3.Image = Properties.Resources.apple;
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.SizeMode = PictureBoxSizeMode.Normal;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // make it full screen
+            this.TopMost = true;
+            this.WindowState = FormWindowState.Maximized;
+            // disable resize button
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            // initial score
+            toolStripStatusLabel1.Text = "Top Score: 0\n     Time: 0";
+            toolStripStatusLabel2.Text = "HIGHSCORE: " + highscore;
+            // play background music
+            wmp1.controls.play(); 
+            wmp1.URL = "background.mp3";
+            wmp1.settings.setMode("loop", true);
+        }
+        
+        void iteration()
+        {
+            if (running)
+            {
+                if (sliceable1)
+                {
+                    sliceable1 = false;
+                    score++;
+                    toolStripStatusLabel1.Text = "Top Score: " + score + "\n     Time: " + counter;
+                    toolStripStatusLabel2.Text = "HIGHSCORE: " + highscore;
+                    // sliced
+                    if (fruit1 == 0) { pictureBox1.Image = Properties.Resources.jabka_s; score++; }
+                    else if (fruit1 == 1) { pictureBox1.Image = Properties.Resources.banana_s; score += 2; }
+                    else if (fruit1 == 2) { pictureBox1.Image = Properties.Resources.jagoda_s; score += 3; }
+                    else if (fruit1 == 3) { pictureBox1.Image = Properties.Resources.praska_s;score += 4; }
+                    if (fruit1 == 4) { pictureBox1.Image = Properties.Resources.lubenica_s; score += 5; }
+                    // sound effect
+                    System.Media.SoundPlayer sp = new System.Media.SoundPlayer();
+                    sp.SoundLocation = "splatter.wav";
+                    sp.Play();
+                }
+                if (sliceable2)
+                {
+                    sliceable2 = false;
+                    score++;
+                    toolStripStatusLabel1.Text = "Top Score: " + score + "\n     Time: " + counter;
+                    toolStripStatusLabel2.Text = "HIGHSCORE: " + highscore;
+                    // sliced
+                    if (fruit2 == 0) pictureBox1.Image = Properties.Resources.jabka_s;
+                    else if (fruit2 == 1) pictureBox1.Image = Properties.Resources.banana_s;
+                    else if (fruit2 == 2) pictureBox1.Image = Properties.Resources.jagoda_s;
+                    else if (fruit2 == 3) pictureBox1.Image = Properties.Resources.praska_s;
+                    if (fruit2 == 4) pictureBox1.Image = Properties.Resources.lubenica_s;
+                    // sound effect
+                    System.Media.SoundPlayer sp = new System.Media.SoundPlayer();
+                    sp.SoundLocation = "splatter.wav";
+                    sp.Play();
+                }
+                if (sliceable3)
+                {
+                    sliceable3 = false;
+                    score++;
+                    toolStripStatusLabel1.Text = "Top Score: " + score + "\n     Time: " + counter;
+                    toolStripStatusLabel2.Text = "HIGHSCORE: " + highscore;
+                    // sliced
+                    if (fruit3 == 0) pictureBox1.Image = Properties.Resources.jabka_s;
+                    else if (fruit3 == 1) pictureBox1.Image = Properties.Resources.banana_s;
+                    else if (fruit3 == 2) pictureBox1.Image = Properties.Resources.jagoda_s;
+                    else if (fruit3 == 3) pictureBox1.Image = Properties.Resources.praska_s;
+                    if (fruit3 == 4) pictureBox1.Image = Properties.Resources.lubenica_s;
+                    // sound effect
+                    System.Media.SoundPlayer sp = new System.Media.SoundPlayer();
+                    sp.SoundLocation = "splatter.wav";
+                    sp.Play();
+                }
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "GAME OVER\nTop Score: " + score + "\n     Time: " + counter;
+                toolStripStatusLabel2.Text = "HIGHSCORE: " + highscore;
+            }
+        }
+
+        void restart()
+        {
+            score = 0;
+            misses = 0;
+            toolStripStatusLabel1.Text = "Top Score: " + score + "\n     Time: " + counter;
+            toolStripStatusLabel2.Text = "HIGHSCORE: " + highscore;
+            running = true;
+            wmp1.controls.play();
+            wmp1.settings.setMode("loop", true);
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // change fruit 
+            int fruits = r.Next(0, 5);
+            fruit1 = r.Next(0, 5);
+            if (fruit1 == 0) pictureBox1.Image = Properties.Resources.apple;
+            else if(fruit1 == 1) pictureBox1.Image = Properties.Resources.banana;
+            else if(fruit1 == 2) pictureBox1.Image = Properties.Resources.basaha;
+            else if(fruit1 == 3) pictureBox1.Image = Properties.Resources.peach;
+            if(fruit1 == 4) pictureBox1.Image = Properties.Resources.sandia;
+            sliceable1 = true;
+            // new position section
+            int x, y;
+            x = r.Next(0, 1420);
+            y = r.Next(0, 650);
+            pictureBox1.Location = new Point(x, y);
+            pictureBox3.Location = new Point(-200, -200);
+            pictureBox2.Location = new Point(-200, -200);
+            if(fruits == 2 || fruits == 3)
+            {
+                fruit2 = r.Next(0, 5);
+                if (fruit2 == 0) pictureBox1.Image = Properties.Resources.apple;
+                else if (fruit2 == 1) pictureBox1.Image = Properties.Resources.banana;
+                else if (fruit2 == 2) pictureBox1.Image = Properties.Resources.basaha;
+                else if (fruit2 == 3) pictureBox1.Image = Properties.Resources.peach;
+                if (fruit2 == 4) pictureBox1.Image = Properties.Resources.sandia;
+                sliceable2 = true;
+                x = r.Next(0, 1420);
+                y = r.Next(0, 650);
+                pictureBox2.Location = new Point(x, y);
+            }
+            else if(fruits == 4)
+            {
+                fruit2 = r.Next(0, 5);
+                if (fruit2 == 0) pictureBox1.Image = Properties.Resources.apple;
+                else if (fruit2 == 1) pictureBox1.Image = Properties.Resources.banana;
+                else if (fruit2 == 2) pictureBox1.Image = Properties.Resources.basaha;
+                else if (fruit2 == 3) pictureBox1.Image = Properties.Resources.peach;
+                if (fruit2 == 4) pictureBox1.Image = Properties.Resources.sandia;
+                sliceable2 = true;
+                x = r.Next(0, 1420);
+                y = r.Next(0, 650);
+                pictureBox2.Location = new Point(x, y);
+                fruit3 = r.Next(0, 4);
+                if (fruit3 == 0) pictureBox1.Image = Properties.Resources.apple;
+                else if (fruit3 == 1) pictureBox1.Image = Properties.Resources.banana;
+                else if (fruit3 == 2) pictureBox1.Image = Properties.Resources.basaha;
+                else if (fruit3 == 3) pictureBox1.Image = Properties.Resources.peach;
+                if (fruit3 == 4) pictureBox1.Image = Properties.Resources.sandia;
+                sliceable3 = true;
+                x = r.Next(0, 1420);
+                y = r.Next(0, 650);
+                pictureBox3.Location = new Point(x, y);
+            }
+            // game over section
+            counter++;
+            if (counter == 30) {
+                timer1.Stop();
+                wmp1.controls.stop();
+                wmp.controls.play();
+                toolStripStatusLabel1.Text = "GAME OVER\nTop Score: " + score + "\n     Time: " + counter ;
+                running = false;
+                if(score > highscore)
+                {
+                    highscore = score;
+                    Settings.Default["highscore"] = score;
+                    Settings.Default.Save();
+                }
+             // replay button section
+                if(flag == true)
+                {
+                    Button button = new Button();
+                    button.Text = "Play again";
+                    button.Click += new System.EventHandler(this.button1_Click);
+                    statusStrip1.Items.Add(new ToolStripControlHost(button));
+                    flag = false;
+                }
+            }
+            toolStripStatusLabel1.Text = "Top Score: " + score + "\n     Time: " + counter;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            restart();
+            counter = 0;
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            // red pill
+            score = score * 2;
+            toolStripStatusLabel1.Text = "Top Score: " + score + "\n     Misses: " + misses;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            // blue pill
+            score = score + 15;
+            toolStripStatusLabel1.Text = "Top Score: " + score + "\n     Misses: " + misses;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void pictureBox1_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            iteration();
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            iteration();
+        }
+
+        private void pictureBox3_MouseMove(object sender, MouseEventArgs e)
+        {
+            iteration();
+        }
+    }
+}
